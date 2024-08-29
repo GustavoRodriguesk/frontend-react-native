b77bb7aaf818ee8b8f2394cb0b54b77e8933758f
GustavoRodriguesk <gustavorodrig780@gmail.com>
2024-08-29 17:34:19 -0300

    ff

--- components/cardAccount.js
+++ components/cardAccount.js
@@ -1,44 +1,27 @@
 import {View, Text, StyleSheet} from 'react-native'
-import { Image } from 'expo-image'
+import CardAccount from './CardAccount'
 
-export default function CardAccount ({ service, userName, imgUrl }) {
-
+export default function Content(){
     return (
-        <View style={styles.card}>
-            <Image 
-                style={styles.logo} 
-                source={imgUrl}
-            />
-            <View style={styles.content}>
-                <Text style={styles.service}>{service}</Text>
-                <Text style={styles.username}>{userName}</Text>
-            </View>
+        <View style={styles.content}>
+
+          <CardAccount />
+          <CardAccount />
+          <CardAccount />
+          <CardAccount />
+          <CardAccount />
+          <CardAccount />
+
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    card: {
-        padding: 10,
-        borderStyle: 'solid',
-        borderColor: '#EEEEEE',
-        borderWidth: 1,
-        flexDirection: 'row',
-        gap: 15,
-        borderRadius: 10,
-        alignItems: 'center'
-    },
-    logo:{
-        width: 60,
-        height: 60
-    },
     content: {
-        gap: 6
-    },
-    service: {
-        fontSize: 17
-    },
-    username: {
-        color: '#777777'
-    }
+        gap: 10,
+        //backgroundColor: "#545656",
+        padding: 15
+        //justifyContent: 'center',
+        //alignItems: 'center'
+      }
 })
